c26a55a10cdb6b95e574eb4ed1bcba46bfda88a8
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 14:31:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,7 +3,7 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-def age_appropriate_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
